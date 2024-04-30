fce22762f04a6c9c015fdd1c51728b43c047df7b
alexfeed1990 <alexfeed1990real@gmail.com>
2024-04-30 17:51:57 +0300

    node.js

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
-import { $ } from "bun";
-const ffmpegPath = require("ffmpeg-static")
+const { exec } = require("node:child_process");
+const multer = require('multer');
+const ffmpegPath = require("ffmpeg-static");
+const express = require("express");
+const fs = require("node:fs");
 
-const server = Bun.serve({
-    async fetch(req) {
-        const path = new URL(req.url).pathname;
-        if (req.method === "POST" && path === "/upload") {
-            const blob = await Bun.readableStreamToBlob(req.body)
-            await Bun.write("temp.png", blob); // Write temp file
+const app = express();
+const port = 3000;
 
-            // Convert video
-            await $`${ffmpegPath} -loop 1 -i temp.png -i money.ogg -shortest -c:v libx264 -tune stillimage -c:a aac -t 16.21 -pix_fmt yuv420p -vf "scale='in_w - mod(in_w, 2):in_h - mod(in_h, 2)',fade=t=in:st=0:d=1.2861" temp.mp4`
-            
-            unlinkSync("temp.png") // Delete temp file
-            return new Response(Bun.file("temp.mp4")); // Return vid
-        }
-
-        // Delete temporary files
-        if (Bun.file("temp.png").exists()) { unlinkSync("temp.png") }
-        if (Bun.file("temp.mp4").exists()) { unlinkSync("temp.mp4") }
-
-        return new Response("Page not found. Post request to /upload with a file.", { status: 404 }); // 404
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './')
+    },
+    filename: function (req, file, cb) {
+        cb(null, 'temp.png')
     }
 })
 
-console.log(`Listening on ${server.url}`);
+const upload = multer({ storage: storage })
+
+app.post('/upload', upload.single('file'), async (req, res) => {
+    if(fs.existsSync("temp.mp4")) fs.unlinkSync("temp.mp4")
+
+    const command = `"${ffmpegPath}" -loop 1 -i temp.png -i money.ogg -shortest -c:v libx264 -tune stillimage -c:a aac -t 16.21 -pix_fmt yuv420p -vf "scale='in_w - mod(in_w, 2):in_h - mod(in_h, 2)',fade=t=in:st=0:d=1.2861" temp.mp4`
+    exec(command).on('close', () => {
+        res.sendFile("temp.mp4", {root: __dirname})
+    })
+})
+
+app.listen(port, () => {
+    console.log("App listening on port " + port + ".");
+})
